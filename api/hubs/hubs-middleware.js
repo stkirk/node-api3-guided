5899d083aa12e6cc2103180c3435ffa142f9feb0
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-29 18:52:33 +0200

    wip

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -4,7 +4,7 @@ async function checkHubId (req, res, next) {
   // if id legit, next()
   // if id bad, next({ not found!!! })
   try {
-
+    const hubMaybe = await Hub.findById()
   } catch (error) {
     next(error)
   }
